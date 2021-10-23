1b5e6b201911a0dd1d908af3e82e6b58dc5cfc34
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2021-10-23 12:23:32 -0400

    fix:deleted babel file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles/global.css';
 import App from './App';
 
-// const _JSXStyle = require('styled-jsx/style').default;
-// if (typeof global !== 'undefined') {
-//     Object.assign(global, { _JSXStyle });
-// }
 
 ReactDOM.render(
   <React.StrictMode>
